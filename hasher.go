4f72b71815470037d8232945a5efdf96062841d3
Harry Law <53987565+h5law@users.noreply.github.com>
2023-06-06 09:03:04 +0100

    Add encoding functions for sum nodes

--- hasher.go
+++ hasher.go
@@ -2,7 +2,9 @@ package smt
 
 import (
 	"bytes"
+	"fmt"
 	"hash"
+	"strconv"
 )
 
 var (
@@ -83,6 +85,10 @@ func (th *treeHasher) parseNode(data []byte) ([]byte, []byte) {
 	return data[len(innerPrefix) : th.hashSize()+len(innerPrefix)], data[len(innerPrefix)+th.hashSize():]
 }
 
+func (th *treeHasher) parseSumNode(data []byte) ([]byte, []byte) {
+	return data[len(innerPrefix) : th.hashSize()+len(innerPrefix)+16], data[len(innerPrefix)+th.hashSize()+16:]
+}
+
 func (th *treeHasher) hashSize() int {
 	return th.hasher.Size()
 }
@@ -103,6 +109,13 @@ func parseLeaf(data []byte, ph PathHasher) ([]byte, []byte) {
 	return data[len(leafPrefix) : ph.PathSize()+len(leafPrefix)], data[len(leafPrefix)+ph.PathSize():]
 }
 
+// parseSumLeaf returns the path, value hash and hex sum of the leaf node
+func parseSumLeaf(data []byte, ph PathHasher) ([]byte, []byte, [16]byte) {
+	var sum [16]byte
+	copy(sum[:], data[len(data)-16:])
+	return data[len(leafPrefix) : ph.PathSize()+len(leafPrefix)], data[len(leafPrefix)+ph.PathSize() : len(data)-16], sum
+}
+
 func parseExtension(data []byte, ph PathHasher) (pathBounds, path, childData []byte) {
 	return data[len(extPrefix) : len(extPrefix)+2],
 		data[len(extPrefix)+2 : len(extPrefix)+2+ph.PathSize()],
@@ -117,12 +130,41 @@ func encodeLeaf(path []byte, leafData []byte) []byte {
 	return value
 }
 
+// encodeSumLeaf returns the pre-image digest of a sum leaf node
+func encodeSumLeaf(path []byte, leafData []byte, sum [16]byte) []byte {
+	value := make([]byte, 0, len(leafPrefix)+len(path)+len(leafData))
+	value = append(value, leafPrefix...)
+	value = append(value, path...)
+	value = append(value, leafData...)
+	value = append(value, sum[:]...)
+	return value
+}
+
 func encodeInner(leftData []byte, rightData []byte) []byte {
 	value := make([]byte, 0, len(innerPrefix)+len(leftData)+len(rightData))
 	value = append(value, innerPrefix...)
 	value = append(value, leftData...)
 	value = append(value, rightData...)
 	return value
+}
+
+// encodeSumInner returns the pre-image digest of the innernode, as well as the sum of its children
+func encodeSumInner(leftData []byte, rightData []byte) ([]byte, [16]byte, error) {
+	value := make([]byte, 0, len(innerPrefix)+len(leftData)+len(rightData))
+	value = append(value, innerPrefix...)
+	value = append(value, leftData...)
+	value = append(value, rightData...)
+	var sum [16]byte
+	leftSum, err := strconv.ParseUint(string(leftData[len(leftData)-16:]), 16, 64)
+	if err != nil {
+		return nil, sum, err
+	}
+	rightSum, err := strconv.ParseUint(string(rightData[len(rightData)-16:]), 16, 64)
+	if err != nil {
+		return nil, sum, err
+	}
+	copy(sum[:], []byte(fmt.Sprintf("%016x", leftSum+rightSum)))
+	return value, sum, nil
 }
 
 func encodeExtension(pathBounds [2]byte, path []byte, childData []byte) []byte {
@@ -132,4 +174,16 @@ func encodeExtension(pathBounds [2]byte, path []byte, childData []byte) []byte {
 	value = append(value, path...)
 	value = append(value, childData...)
 	return value
+}
+
+// encodeSumExtension returns the pre-image digest of the extension node and the sum of its child
+func encodeSumExtension(pathBounds [2]byte, path []byte, childData []byte) ([]byte, [16]byte) {
+	value := make([]byte, 0, len(extPrefix)+len(path)+2+len(childData))
+	value = append(value, extPrefix...)
+	value = append(value, pathBounds[:]...)
+	value = append(value, path...)
+	value = append(value, childData...)
+	var sum [16]byte
+	copy(sum[:], childData[len(childData)-16:])
+	return value, sum
 }
