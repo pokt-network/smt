268a6d80c658882104d5cccdba18c38a00c67284
harry <53987565+h5law@users.noreply.github.com>
2023-10-04 19:50:11 +0100

    chore: add bitmask comment

--- proofs.go
+++ proofs.go
@@ -142,6 +142,9 @@ func (proof *SparseCompactMerkleProof) validateBasic(spec *TreeSpec) error {
 
 	// Compact proofs: check that the length of the bit mask is as expected
 	// according to NumSideNodes.
+	// number of bytes needed to represent the number of side nodes
+	// for example: 1 byte is needed to represent 8 side nodes
+	//			    32 bytes are needed to represent 256 side nodes
 	bml := int(math.Ceil(float64(proof.NumSideNodes) / float64(8)))
 	if len(proof.BitMask) != bml {
 		return fmt.Errorf("invalid bit mask length: got %d want %d", len(proof.BitMask), bml)
