b1fa1116f5bc46d2aeae7c5516bcab865a1af87c
Bryan White <bryanchriswhite@gmail.com>
2024-07-16 13:51:44 +0200

    Revert "refactor: move applicable root methods to MerkleRoot and inherit in MerkleSumRoot"
    This reverts commit a7a3f71960af7f79a8ad9ba26079305e9e03fdce.

--- root.go
+++ root.go
@@ -5,40 +5,6 @@ import (
 	"fmt"
 )
 
-// MustCount returns the uint64 count of the merkle root, a cryptographically secure
-// count of the number of non-empty leafs in the tree. It panics if the root length
-// is invalid.
-func (root MerkleRoot) MustCount() uint64 {
-	count, err := root.Count()
-	if err != nil {
-		panic(err)
-	}
-
-	return count
-}
-
-// Count returns the uint64 count of the merkle root, a cryptographically secure
-// count of the number of non-empty leafs in the tree. It returns an error if the
-// root length is invalid.
-func (root MerkleRoot) Count() (uint64, error) {
-	if err := root.validateBasic(); err != nil {
-		return 0, err
-	}
-
-	return root.count(), nil
-}
-
-// DigestSize returns the length of the digest portion of the root.
-func (root MerkleRoot) DigestSize() int {
-	return len(root) - countSizeBytes - sumSizeBytes
-}
-
-// HasDigestSize returns true if the root digest size is the same as
-// that of the size of the given hasher.
-func (root MerkleRoot) HasDigestSize(size int) bool {
-	return root.DigestSize() == size
-}
-
 // MustSum returns the uint64 sum of the merkle root, it checks the length of the
 // merkle root and if it is no the same as the size of the SMST's expected
 // root hash it will panic.
@@ -62,8 +28,42 @@ func (root MerkleSumRoot) Sum() (uint64, error) {
 	return root.sum(), nil
 }
 
+// MustCount returns the uint64 count of the merkle root, a cryptographically secure
+// count of the number of non-empty leafs in the tree. It panics if the root length
+// is invalid.
+func (root MerkleSumRoot) MustCount() uint64 {
+	count, err := root.Count()
+	if err != nil {
+		panic(err)
+	}
+
+	return count
+}
+
+// Count returns the uint64 count of the merkle root, a cryptographically secure
+// count of the number of non-empty leafs in the tree. It returns an error if the
+// root length is invalid.
+func (root MerkleSumRoot) Count() (uint64, error) {
+	if err := root.validateBasic(); err != nil {
+		return 0, err
+	}
+
+	return root.count(), nil
+}
+
+// DigestSize returns the length of the digest portion of the root.
+func (root MerkleSumRoot) DigestSize() int {
+	return len(root) - countSizeBytes - sumSizeBytes
+}
+
+// HasDigestSize returns true if the root digest size is the same as
+// that of the size of the given hasher.
+func (root MerkleSumRoot) HasDigestSize(size int) bool {
+	return root.DigestSize() == size
+}
+
 // validateBasic returns an error if the root digest size is not a power of two.
-func (root MerkleRoot) validateBasic() error {
+func (root MerkleSumRoot) validateBasic() error {
 	if !isPowerOfTwo(root.DigestSize()) {
 		return fmt.Errorf("MerkleSumRoot#validateBasic: invalid root length")
 	}
@@ -71,18 +71,18 @@ func (root MerkleRoot) validateBasic() error {
 	return nil
 }
 
-// count returns the count of the node stored in the root.
-func (root MerkleRoot) count() uint64 {
-	_, firstCountByteIdx := getFirstMetaByteIdx(root)
-
-	return binary.BigEndian.Uint64(root[firstCountByteIdx:])
-}
-
 // sum returns the sum of the node stored in the root.
 func (root MerkleSumRoot) sum() uint64 {
 	firstSumByteIdx, firstCountByteIdx := getFirstMetaByteIdx(root)
 
 	return binary.BigEndian.Uint64(root[firstSumByteIdx:firstCountByteIdx])
+}
+
+// count returns the count of the node stored in the root.
+func (root MerkleSumRoot) count() uint64 {
+	_, firstCountByteIdx := getFirstMetaByteIdx(root)
+
+	return binary.BigEndian.Uint64(root[firstCountByteIdx:])
 }
 
 // isPowerOfTwo function returns true if the input n is a power of 2
